0e268cec20a8991e521fef1e9409fdaa21cfd026
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-02-28 17:08:47 -0300

    Add contact cards

---
+++ src/app/components/contact-option/contact-option.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-option',
+  imports: [],
+  templateUrl: './contact-option.component.html',
+  styleUrl: './contact-option.component.scss'
+})
+export class ContactOptionComponent {
+
+}
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContactOptionComponent } from '../contact-option/contact-option.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [],
+  imports: [ContactOptionComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
