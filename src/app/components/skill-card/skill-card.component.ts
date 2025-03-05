c5089f7b9489a2efd5c39994bfa9649ad307f805
araujoviana <araujomatheusgabriel1@gmail.com>
2025-03-05 17:18:00 -0300

    Shine gradient in cards

--- src/app/components/skill-card/skill-card.component.ts
+++ src/app/components/skill-card/skill-card.component.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-skill-card',
@@ -11,5 +11,21 @@ export class SkillCardComponent {
   @Input() name!: string;
   @Input() description!: string;
   @Input() icon!: string;
-  @Input() usageMedia!: string; 
+  @Input() usageMedia!: string;
+
+  x: number = 0;
+  y: number = 0;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    this.x = event.clientX - rect.left;
+    this.y = event.clientY - rect.top;
+
+    this.el.nativeElement.style.setProperty('--x', `${this.x}px`);
+    this.el.nativeElement.style.setProperty('--y', `${this.y}px`);
+    console.log(this.x,this.y);
+  }
 }
