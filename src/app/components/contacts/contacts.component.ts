604e9af805f242a54652b672705b2566e779c58d
araujoviana <araujomatheusgabriel1@gmail.com>
2025-03-06 00:05:26 -0300

    Write contacts!

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { ContactOptionComponent } from '../contact-option/contact-option.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
-  imports: [ContactOptionComponent],
+  imports: [NgOptimizedImage],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
+  linkedinLink: string = "https://www.linkedin.com/in/matheus-gabriel-viana-592685281/";
+  githubLink: string = "https://github.com/araujoviana";
+  emailLink: string = "vianamatheusgabriel1@gmail.com";
 
+  emailText: string = "Copy Email";
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.emailLink).then(r => console.log(r));
+    this.emailText = "Email copied!";
+  }
 }
