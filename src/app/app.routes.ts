038feff19811af64e2e149ace2e455497c0686d5
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-03-11 15:57:33 -0300

    Finish first version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 
 export const routes: Routes = [
-    {path:'', title:'Welcome!', component: HomeComponent},
+    {path:'', title:'Matheus Viana | Portfolio', component: HomeComponent},
     {path:'about', title:'About Me', component: AboutComponent},
     {path:'projects', title:'Projects', component: ProjectsComponent},
     {path:'skills', title:'Skills', component: SkillsComponent},
-    {path:'contacts', title:'Let\'s connect!', component: ContactsComponent},
+    {path:'contacts', title:'Let\'s Connect', component: ContactsComponent},
     {path:'**', title:'Welcome!', component: HomeComponent},
 ];
